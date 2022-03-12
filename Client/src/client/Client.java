b2d44751da5647aa0c6afa16f58ffb43faeeb29a
Sudo-Rahman <keturdu71880@gmail.com>
2022-03-12 09:08:03 +0100

    comiit linux.

---
+++ Client/src/client/Client.java
@@ -0,0 +1,134 @@
+package client;
+
+/*
+ * Nom de classe : Client
+ *
+ * Description   : Cette classe jouera le role de client, il se connectera au serveur, et lui enverra des requetes.
+ *
+ * Version       : 1.0, 1.1
+ *
+ * Date          : 22/02/2022, 24/02/2022
+ *
+ * Copyright     : Yilmaz Rahman, Colliat Maxime
+ *
+ */
+
+
+import utils.Log;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Client
+{
+    private final String username;
+    private Socket socket;
+    private static final Log log = new Log();
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public Client(Socket socket, String username)
+    {
+        this.username = username;
+        try
+        {
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Lance la connexion au serveur, envoie et ecoute les messages du serveur.
+     */
+    public void startClient()
+    {
+        envoieMessage(this.username);
+        listenSocket();
+        String message;
+        while (!this.socket.isClosed() && this.socket.isConnected())
+        {
+            Scanner sn = new Scanner(System.in);
+            message = sn.nextLine();
+            if (message.equals("-1"))
+            {
+                closeAll();
+                log.info("Vous vous etes deconnecter");
+            } else envoieMessage(message);
+        }
+    }
+
+
+    /**
+     * intercepte les messages envoyé par le serveur et les affiche dans la console.
+     */
+    private void litMess()
+    {
+        String mess;
+        // permet d'intercepter tout le message y compris si ya des sauts de ligne tant que le serveur est connecté et qu'il y a un message.
+        while (!this.socket.isClosed() && this.socket.isConnected())
+        {
+            try
+            {
+                if ((mess = bufferedReader.readLine()) != null) System.out.println(mess);
+                else// si le serveur a été fermé par force et que son socket n'a pas été fermé il enverra que des null, donc on quitte le programme
+                {
+                    log.fatal("Le serveur ne repond pas !!");
+                    closeAll();
+                    System.exit(-1);
+                }
+
+            } catch (IOException e)
+            {
+                closeAll();
+                break;
+            }
+        }
+    }
+
+
+    /**
+     * envoie le message en parametre au serveur.
+     */
+    private void envoieMessage(String message)
+    {
+        try
+        {
+            this.bufferedWriter.write(message);
+            this.bufferedWriter.newLine();
+            this.bufferedWriter.flush();
+
+        } catch (IOException e)
+        {
+            log.fatal("Le serveur ne repond pas !!");
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * Thread qui ecoute les message venant du serveur, ainsi le client n'est pas bloqué uniquement sur l'écoute.
+     */
+    private void listenSocket()
+    {
+        Thread ecouteurBluetooth = new Thread(this::litMess);
+        ecouteurBluetooth.start();
+    }
+
+    private void closeAll()
+    {
+        try
+        {
+            this.socket.close();
+            this.bufferedReader.close();
+            this.bufferedWriter.close();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Client/src/utils/InitParametre.java
@@ -0,0 +1,123 @@
+/*
+ * Nom de classe : InitParametre
+ *
+ * Description   : Classe qui contient des fonctions pour initialisé les ports, sockets et quelques attributs des
+ * classes Clients et Serveur.
+ *
+ * Version       : 1.0
+ *
+ * Date          : 24/02/2022
+ *
+ * Copyright     : Yilmaz Rahman, Colliat Maxime
+ *
+ */
+
+package utils;
+
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class InitParametre
+{
+    private static final Log log = new Log();
+
+    /**
+     * initialise le socket du serveur.
+     */
+    public static ServerSocket initServer()
+    {
+        int port = initPort();
+        ServerSocket socketServeur;
+        try
+        {
+            socketServeur = new ServerSocket(port);
+            return socketServeur;
+        } catch (IOException e)
+        {
+            log.error("Le port ne marche pas");
+            System.exit(-1);
+        }
+        return null;
+    }
+
+
+    /**
+     * initialise le socket de connexion au serveur.
+     */
+    public static Socket initSocket()
+    {
+        int port = initPort();
+        InetAddress ip = initIp();
+        Socket socket;
+        try
+        {
+            socket = new Socket(ip, port);
+            return socket;
+        } catch (IOException e)
+        {
+            log.error("Le socket n'a pas pu etres initialisé");
+            System.exit(-1);
+        }
+        return null;
+    }
+
+    /**
+     * initialisation du nombre maximal de clients que le serveur principale pourra taiter en meme temps.
+     */
+    public static int initMaxClients()
+    {
+        int maxClients = 0;
+        int nbThreads = Runtime.getRuntime().availableProcessors() / 4; // ici on a choisit d'alouer 4 thread au mini
+        // pour le calcule pur et dur, pas les ecoutes,et envoies.
+        while (maxClients < 1 || maxClients > nbThreads)
+        {
+            System.out.println("Donner le nombre max de clients en simultané sachant que le serveur alloue 4 threads " +
+                    "au minimum par clients, le nombre max de client est : " + nbThreads);
+            Scanner sn = new Scanner(System.in);
+            maxClients = Integer.parseInt(sn.next());
+        }
+        return maxClients;
+    }
+
+
+    /**
+     * initialisation d'un port pour un socket.
+     */
+    private static int initPort()
+    {
+        int port = 0;
+        while (65535 < port || port < 1024) // Un port est identifié par un entier de 1 à 65535. Par convention les
+            // 1024 premiers sont réservés pour des services standard
+        {
+            System.out.print("Donner le port de connexion (1025 à 65535) : ");
+            Scanner sn = new Scanner(System.in);
+            port = Integer.parseInt(sn.next());
+        }
+        return port;
+    }
+
+    /**
+     * initialisation de l'adresse ip de connexion.
+     */
+    private static InetAddress initIp()
+    {
+        InetAddress ipserveur;
+        System.out.print("Donner l'adresse ip de connexion : ");
+        Scanner sn = new Scanner(System.in);
+        try
+        {
+            ipserveur = InetAddress.getByName(sn.next());
+            return ipserveur;
+
+        } catch (UnknownHostException e)
+        {
+            log.error("L'adresse ip n'est pas bon.");
+            return initIp();
+        }
+    }
+}
